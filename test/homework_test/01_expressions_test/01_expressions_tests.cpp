17d4a70bd2c88ea26b88bf77c83b7c6ba075d06f
Davmill13 <david.miller2@g.austincc.edu>
2020-01-28 19:22:59 -0600

    Homework

--- test/homework_test/01_expressions_test/01_expressions_tests.cpp
+++ test/homework_test/01_expressions_test/01_expressions_tests.cpp
@@ -12,3 +12,13 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("verify multily_numbers fuction")
+{
+	REQUIRE(multiply_numbers(2, 2) == 4);
+	REQUIRE(multiply_numbers(3, 3) == 9);
+	REQUIRE(multiply_numbers(5, 5) == 25);
+}
+
+
+
+
