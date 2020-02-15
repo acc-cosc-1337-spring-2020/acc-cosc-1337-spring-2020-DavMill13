476e06d0a36e3fc60688d0fb700d80c2c73a30dc
David Miller <Davmille13@aol.com>
2020-02-14 23:46:03 -0600

    got a little lost on hw2

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -1,7 +1,17 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "decisions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test get grade points")
+{	
+	REQUIRE(get_grade_points();
+}
+
+TEST_CASE("Test calculate GPA")
+{
+	REQUIRE(calculate_gpa());
+}
