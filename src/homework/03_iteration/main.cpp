bc7a0416b7f89ae84321dc0390b75a24dabb1785
David Miller <Davmille13@aol.com>
2020-02-23 21:31:00 -0600

    Did my best but kept runing into issues with the tests

--- src/homework/03_iteration/main.cpp
+++ src/homework/03_iteration/main.cpp
@@ -1,7 +1,11 @@
+#include<string>
+#include "dna.h"
+#include "dna.cpp"
+#include<iostream>
 //write include statements
 
 //write using statements
-
+using namespace std;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +16,76 @@ user enters a y or Y.
 */
 int main() 
 {
+
+
+	int option;
+
+	string dna;
+
+	while (1) 
+	{
+
+
+		cout << "Enter a DNA String: ";
+
+		cin >> dna;
+
+
+		cout << "\nSelect option" << endl;
+
+		cout << "1. GC Content" << endl;
+
+		cout << "2. DNA Complement\n" << endl;
+
+		cout << "Enter option: ";
+
+		cin >> option;
+
+		if (option == 1) 
+		{
+
+			double content = get_gc_content(dna);
+
+			cout << "GC content: " << content << endl;
+
+		}
+		else if (option == 2)
+		{
+
+			string complement = get_dna_complement(dna);
+
+			cout << "Complement: " << complement << endl;
+
+		}
+		else 
+		{
+
+			cout << "Invalid input!" << endl;
+
+		}
+		char ch;
+
+		cout << "\nTo continue preess (Y/y): ";
+
+		cin >> ch;
+		
+		if (ch == 'Y' || ch == 'y') 
+		{
+
+			cout << "\n" << endl;
+
+			continue;
+
+		}
+		else
+		{
+
+			break;
+
+		}
+
+	}
+
 	return 0;
+
 }
