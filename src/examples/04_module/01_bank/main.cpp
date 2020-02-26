e28e7f10ccdb68a2f6a4019efcb7bf616f70ff12
Davmill13 <david.miller2@g.austincc.edu>
2020-02-25 19:14:35 -0600

    bank deposit

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,6 +1,31 @@
+#include "bank_account.h"
+#include<iostream>
+
+using std::cout;
+using std::cin;
+
 
 int main()
 {
+	auto num{ 0 };
+	auto num1{ 1.5 };
+	auto c{ 'c' };
+	auto flag{ true };
 
+
+	BankAccount account(500);
+	auto balance = account.get_balance();
+	cout << "balance is: " << balance;
+
+	auto amount{ 0 };
+	cout << "\nEnter deposit amount: \n";
+	cin >> amount;
+
+
+	account.deposit(amount);
+	cout << "Balance is: " << account.get_balance();
+
+
+	
 	return 0;
 }
