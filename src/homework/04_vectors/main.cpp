a620db0360435edc8711915a3655d344bccbb407
David Miller <Davmille13@aol.com>
2020-03-01 20:05:09 -0600

    hope these are right

--- src/homework/04_vectors/main.cpp
+++ src/homework/04_vectors/main.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<vector>
+#include "vectors.h"
+#include "vectors.cpp"
 /*
 use a vector of int with values 8, 4, 20, 88, 66, 99
 
@@ -7,8 +11,73 @@ and display them to screen.
 Program continues until user decides to exit.
 
 */
-int main() 
+int main()
+
 {
 
+	vector<int> numbers = { 8,4,20,88,66,99 };
+
+	vector<int> result;
+
+	int choice = 0, num, i;
+
+	while (choice != -1)
+	{
+
+		cout << "Enter 1 to Get Max from vector and 2 for Get primes\n";
+
+		cout << "Enter -1 to exit\n";
+
+		cout << "Enter your choice:";
+
+		cin >> choice;
+
+
+
+		switch (choice)
+
+		{
+
+		case 1:cout << "Maximum value in the vector is " << get_max_from_vector(numbers) << endl << endl;
+
+			break;
+
+		case 2:cout << "Enter number to get primes till that number:";
+
+			cin >> num;
+
+			result = vector_of_primes(num);
+
+			cout << "\nvector with values ";
+
+			for (i = 0; i < result.size(); i++)
+
+			{
+
+				if (i == result.size() - 1)
+
+					cout << result[i];
+
+				else
+
+					cout << result[i] << ", ";
+
+
+
+			}
+
+			cout << "\n\n";
+
+			break;
+
+		case -1:exit(0);
+
+		default:cout << "Invalid choice";
+
+		}
+
+	}
+
 	return 0;
+
 }
