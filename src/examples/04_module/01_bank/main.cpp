3e674ff70c63d96e3969237c659be04ceeea293c
Davmill13 <david.miller2@g.austincc.edu>
2020-03-05 18:49:47 -0600

    claswork for the day

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -8,25 +8,20 @@ using std::cin;
 
 int main()
 {
-
-	auto num{ 0 };
-	auto num1{ 1.5 };
-	auto c{ 'c' };
-	auto flag{ true };
-
-	BankAccount a;
-	cout << a.get_balance();
-
-	std::vector<BankAccount> accounts{ BankAccount(100),
+	/*std::vector<BankAccount> accounts{ BankAccount(100),
 		BankAccount(200) };
 	for (auto act : accounts)
 	{
 		cout << act.get_balance() << "\n";
-	}
+	}*/
 
 	BankAccount account(500);
+	cin >> account;
+	cout << account;
+	display_balance(account);
+
 	auto balance = account.get_balance();
-	cout << "balance is: " << balance;
+	cout << "balance is: \n" << balance;
 
 	auto amount{ 0 };
 	cout << "\nEnter deposit amount: \n";
